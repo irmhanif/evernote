3b44de293a45b13af71c3d6e431c70fe57fac5fe
Irm <idrishan1996@gmail.com>
2023-10-15 01:16:00 +0530

    Side Bar base layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-
-
-function App() {
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  StarFilled,
+  UserOutlined,
+  HomeFilled,
+  PlusOutlined,
+  FileTextFilled,
+  CheckCircleFilled
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+const { Sider } = Layout;
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Layout >
+      <Sider trigger={null} collapsible collapsed={collapsed} style={{height: '100vh'}}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Mohamed Idris M',
+            },
+            {
+              key: '2',
+              icon: <PlusOutlined />,
+              label: 'New',
+            },
+            {
+              key: '3',
+              icon: <HomeFilled />,
+              label: 'Home',
+            },
+            {
+              key: '4',
+              icon: <StarFilled />,
+              label: 'Shortcuts',
+            },
+            {
+              key: '5',
+              icon: <FileTextFilled />,
+              label: 'Notes',
+            },
+            {
+              key: '6',
+              icon: <CheckCircleFilled />,
+              label: 'Tasks',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        
+      </Layout>
+    </Layout>
   );
-}
-
+};
 export default App;
