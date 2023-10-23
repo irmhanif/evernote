d9b75a9464f602b9706d46972cd312003f70dc11
Irm <idrishan1996@gmail.com>
2023-10-24 02:13:05 +0530

    Sidebar resizer

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import cs from 'classnames';
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
     StarFilled,
     HomeFilled,
     PlusOutlined,
@@ -9,58 +8,127 @@ import {
     CheckCircleFilled
 } from '@ant-design/icons';
 import { Layout, Menu, Button } from 'antd';
+import {
+    BsChevronLeft,
+    BsChevronRight
+} from "react-icons/bs";
 import Profile from './Profile';
 
 function SideBar() {
     const [collapsed, setCollapsed] = useState(false);
     const { Sider } = Layout;
+    const [isResizing, setIsResizing] = useState(false);
+    const [width, setWidth] = useState(240); // Initial width
+    const minWidth = 210; // Minimum width
+    const maxWidth = 400; // Maximum width
+    const thresholdWidth = 100; // Width threshold for auto-collapse
+    const collapsedWidth = 80; // Width when collapsed
+
+
+    const handleMouseDown = () => {
+        setIsResizing(true);
+    };
+
+    const handleMouseUp = () => {
+        setIsResizing(false);
+    };
+
+    const handleMouseMove = (e) => {
+        if (isResizing) {
+            let newWidth = e.clientX;
+            // Enforce minimum and maximum width
+            if (newWidth < thresholdWidth) {
+                setWidth(collapsedWidth);
+                setCollapsed(true)
+            } else if(newWidth>thresholdWidth && newWidth<= minWidth) {
+                newWidth=210
+                setCollapsed(false)
+            }
+            if (newWidth >= minWidth && newWidth <= maxWidth) {
+                setWidth(newWidth);
+            }
+            
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isResizing]);
+
     return (
-        <Sider trigger={null} collapsible collapsed={collapsed} style={{ height: '100vh' }} className='bg-primary'>
+        <Sider
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            style={{
+                height: '100vh',
+                minWidth: `${minWidth}px`, // Minimum width
+                maxWidth: `${maxWidth}px`, // Maximum width
+                boxSizing: 'border-box'
+            }}
+            width={width}
+            className={cs('bg-primary group relative', collapsed && 'reducedSideBar')}
+        >
+            <div
+                className={cs('sideBarResizer')}
+                onMouseDown={handleMouseDown}
+            />
             <div className="demo-logo-vertical" />
-            <Profile />
-            <Menu
-                theme="dark"
-                className='bg-primary'
-                mode="inline"
-                defaultSelectedKeys={['2']}
-                items={[
-                    {
-                        key: '2',
-                        icon: <PlusOutlined />,
-                        label: 'New',
-                    },
-                    {
-                        key: '3',
-                        icon: <HomeFilled />,
-                        label: 'Home',
-                    },
-                    {
-                        key: '4',
-                        icon: <StarFilled />,
-                        label: 'Shortcuts',
-                    },
-                    {
-                        key: '5',
-                        icon: <FileTextFilled />,
-                        label: 'Notes',
-                    },
-                    {
-                        key: '6',
-                        icon: <CheckCircleFilled />,
-                        label: 'Tasks',
-                    },
-                ]}
-            />
-            <Button
-                type="text"
-                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                onClick={() => setCollapsed(!collapsed)}
-                style={{
-                    fontSize: '16px',
-                    width: 64,
-                    height: 64,
-                }}
-            />
+            <div style={{ userSelect: 'none' }}>
+                <Profile collapsed={!collapsed} />
+                <Menu
+                    theme="dark"
+                    className='bg-primary'
+                    mode="inline"
+                    defaultSelectedKeys={['2']}
+                    items={[
+                        {
+                            key: '2',
+                            icon: <PlusOutlined />,
+                            label: 'New',
+                        },
+                        {
+                            key: '3',
+                            icon: <HomeFilled />,
+                            label: 'Home',
+                        },
+                        {
+                            key: '4',
+                            icon: <StarFilled />,
+                            label: 'Shortcuts',
+                        },
+                        {
+                            key: '5',
+                            icon: <FileTextFilled />,
+                            label: 'Notes',
+                        },
+                        {
+                            key: '6',
+                            icon: <CheckCircleFilled />,
+                            label: 'Tasks',
+                        },
+                    ]}
+                />
+                <Button
+                    type="text"
+                    icon={collapsed ? <BsChevronRight /> : <BsChevronLeft />}
+                    className={cs('collapseButton hidden group-hover:inline-block ')}
+                    onClick={() => {
+                        setCollapsed(!collapsed)
+                        setWidth(240)
+                    }}
+                    style={{
+                        color: '#fff'
+                    }}
+                />
+            </div>
         </Sider>
     )
 }
