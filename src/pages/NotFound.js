1451ba1a54e87f9cdd226a3d9ce75c005a4d4b3a
Irm <idrishan1996@gmail.com>
2023-10-24 04:00:03 +0530

    404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ReactComponent as NotFoundSVG} from '../assets/404.svg'
+
+function NotFound() {
+  return (
+    <NotFoundSVG height={'100vh'} />
+  )
+}
+
+export default NotFound
