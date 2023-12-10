d942e2d1f902a49e8a2eb3254164e9c1e3b24629
Irm <idriswan@gmail.com>
2023-12-10 09:49:40 +0530

    HomeGrid

--- src/components/HomeGrid.js
+++ src/components/HomeGrid.js
@@ -1,71 +1,72 @@
 import React, { useState } from 'react';
-import GridLayout, { WidthProvider } from 'react-grid-layout';
+import { WidthProvider, Responsive } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
-const ReactGridLayout = WidthProvider(GridLayout);
+const ReactGridLayout = WidthProvider(Responsive);
 
 const HomeGrid = () => {
     const [layout, setLayout] = useState([
-        { i: 'notes', x: 0, y: 0, w: 10, h: 4 },
-        { i: 'scratchPad', x: 10, y: 0, w: 6, h: 4 },
-        { i: 'recentlyCaptured', x: 0, y: 4, w: 16, h: 4 },
+        { i: 'notes', x: 0, y: 0, w: 8, h: 8 },
+        { i: 'scratchPad', x: 10, y: 0, w: 4, h: 8 },
+        { i: 'recentlyCaptured', x: 0, y: 4, w: 12, h: 14 },
     ]);
+
+    const [isDraggable, setIsDraggable] = useState(false);
 
     const onLayoutChange = (newLayout) => {
         setLayout(newLayout);
     };
 
-    const onWidthChange = (i, newWidth) => {
-        // Update the width of the selected item
-        const updatedLayout = layout.map(item =>
-            item.i === i ? { ...item, w: newWidth } : item
-        );
+    const handleReOrderClick = () => {
+        setIsDraggable(!isDraggable);
+    };
 
-        // Calculate the remaining width to distribute among nearby siblings
-        const remainingWidth = 16 - newWidth;
-
-        // Update the width of nearby sibling elements
-        const updatedLayoutWithReducedWidth = updatedLayout.map(item =>
-            item.i !== i ? { ...item, w: (item.w / (16 - newWidth)) * remainingWidth } : item
-        );
-
-        setLayout(updatedLayoutWithReducedWidth);
+    const handleSaveClick = () => {
+        console.log(layout);
     };
 
     return (
-        <ReactGridLayout
-            className="layout"
-            layout={layout}
-            cols={16}
-            rowHeight={30}
-            width={1200}
-            draggableHandle=".drag-handle"
-            onLayoutChange={onLayoutChange}
-            resizeHandles={['s', 'w', 'e', 'sw', 'se']}
-            onResizeStop={(layout, oldItem, newItem) => onWidthChange(newItem.i, newItem.w)}
-        >
-            <div key="notes" className="bg-white p-4 shadow-md rounded-md">
-                <h2 className="text-2xl font-bold mb-4 drag-handle">Notes</h2>
-                {/* Your Notes content goes here */}
-                {/* Example content */}
-                <p>This is the content of the Notes container.</p>
+        <>
+            <div className='hidden'>
+                <button onClick={handleReOrderClick}>ReOrder</button>
+                <button onClick={handleSaveClick}>Save</button>
             </div>
+            <ReactGridLayout
+                className="layout gridSection"
+                layouts={{ lg: layout }}
+                onLayoutChange={onLayoutChange}
+                isDraggable={isDraggable}
+                isResizable={isDraggable}
+                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                rowHeight={30}
+                width={1200}
+                draggableHandle={null}
+                resizeHandles={['s', 'w', 'e', 'sw', 'se']}
+            >
+                <div key="notes" className="bg-white p-4 shadow-md rounded-md">
+                    <h2 className="text-2xl font-bold mb-4 ">Notes</h2>
+                    {/* Your Notes content goes here */}
+                    {/* Example content */}
+                    <p>This is the content of the Notes container.</p>
+                </div>
 
-            <div key="scratchPad" className="bg-white p-4 shadow-md rounded-md">
-                <h2 className="text-2xl font-bold mb-4 drag-handle">Scratch Pad</h2>
-                {/* Your Scratch Pad content goes here */}
-                {/* Example content */}
-                <p>This is the content of the Scratch Pad container.</p>
-            </div>
+                <div key="scratchPad" className="bg-white p-4 shadow-md rounded-md">
+                    <h2 className="text-2xl font-bold mb-4 ">Scratch Pad</h2>
+                    {/* Your Scratch Pad content goes here */}
+                    {/* Example content */}
+                    <p>This is the content of the Scratch Pad container.</p>
+                </div>
 
-            <div key="recentlyCaptured" className="bg-white p-4 shadow-md rounded-md">
-                <h2 className="text-2xl font-bold mb-4 drag-handle">Recently Captured</h2>
-                {/* Your Recently Captured content goes here */}
-                {/* Example content */}
-                <p>This is the content of the Recently Captured container.</p>
-            </div>
-        </ReactGridLayout>
+                <div key="recentlyCaptured" className="bg-white p-4 shadow-md rounded-md">
+                    <h2 className="text-2xl font-bold mb-4 ">Recently Captured</h2>
+                    {/* Your Recently Captured content goes here */}
+                    {/* Example content */}
+                    <p>This is the content of the Recently Captured container.</p>
+                </div>
+            </ReactGridLayout >
+        </>
+
     );
 };
 
