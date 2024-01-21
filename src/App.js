7bbf4f803e8ac267453b11859a83f137607370d5
Irm <idriswan@gmail.com>
2024-01-22 02:07:17 +0530

    Route fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route as RouteV6, Routes, Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Route as RouteV6, Routes, Navigate, useNavigate } from 'react-router-dom'
 import './App.scss';
 import { theme } from 'antd';
 import Login from './pages/Login';
@@ -13,23 +13,28 @@ const App = () => {
   // Your logic to check if the user is logged in
   // This is just a placeholder; you should replace it with your actual authentication logic
 
-  return (
-    <>
-      {/* <Dashboard /> */}
-      <Router>
-        <div style={{ background: colorBgContainer }}>
-          <Routes>
-            {/* <RouteV6 path="/" element={<Navigate to="/projects/featherNotes" />} /> */}
-            <RouteV6 path="/projects/featherNotes/login" element={<Login />} />
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (checkLoginStatus()) {
+      navigate('/projects/featherNotes');
+    } else {
+      navigate('/projects/featherNotes/login');
+    }
+  }, [])
 
-            <RouteV6
-              path="/projects/featherNotes"
-              element={checkLoginStatus() ? <Dashboard /> : <Navigate to="/projects/featherNotes/login" />}
-            />
-            <RouteV6 path="*" element={checkLoginStatus() ? <Dashboard /> : <Navigate to="/projects/featherNotes/login" />} />
-          </Routes>
-        </div>
-      </Router></>
+  return (
+    <div style={{ background: colorBgContainer }}>
+      <Routes>
+        {/* <RouteV6 path="/" element={<Navigate to="/projects/featherNotes" />} /> */}
+        <RouteV6 path="/projects/featherNotes/login" element={<Login />} />
+
+        <RouteV6
+          path="/projects/featherNotes"
+          element={<Dashboard />}
+        />
+        <RouteV6 path="*" element={checkLoginStatus() ? <Dashboard /> : <Navigate to="/projects/featherNotes/login" />} />
+      </Routes>
+    </div>
   );
 };
 export default App;
